3608b030eee7156c269603636ebfc25b8f1ab49a
Soyaib10 <soyaibzihad10@gmail.com>
2025-06-13 22:34:21 +0600

    add config, application struct, default router and a health checker

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware. At the moment this only contains a copy of the config struct and a
+// logger, but it will grow to include a lot more as our build progresses.
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var cfg config
+
+	// command line flags
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	// Start the HTTP server.
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
