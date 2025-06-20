081ea8be3be716864afdbbe69a09e408a29fd3e4
Soyaib10 <soyaibzihad10@gmail.com>
2025-06-20 11:50:20 +0600

    add router and healthcheck

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,8 +10,6 @@ func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Get("/v1/healthcheck", app.healthcheckHandler)
-	// r.Get("/v1/movies", app.moviesHandler)
-	// r.Get("/v1/movie{id}", app.movieHandler)
 
 	return r
 }
