fbf34d3cd6983a1b87cd2a15d1bdcd158e6a8195
Soyaib10 <soyaibzihad10@gmail.com>
2025-08-01 21:21:08 +0600

    data validation added for movie struct

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 1. panic vs return- when to do what? page 87
 
-# Restricting inputs
+# 4.3- Restricting inputs
+- add error handling for checking unknown fileds, multiple json and large files more than one mb
+
+# 4.4- Custom JSON Decoding
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/Soyaib10/popcorndb-api/internal/data"
+	"github.com/Soyaib10/popcorndb-api/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +18,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.badRequestResponse(w, err)
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title: input.Title,
+		Year: input.Year,
+		Runtime: input.Runtime,
+		Genres: input.Genres,
+	}
+
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
@@ -43,6 +57,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.logger.Println(err)
-		app.serverErrorResponse(w, err)
+		app.serverErrorResponse(w, r, err)
 	}
 }
